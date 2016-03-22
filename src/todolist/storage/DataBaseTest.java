cd4bad4d93be648b86ff3ffffff023f53a78898b
xiao.yuxin.2012@vjc.sg <xiao.yuxin.2012@vjc.sg>
2016-03-22 21:52:23 +0800

    add unit testing

---
+++ src/todolist/storage/DataBaseTest.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package todolist.storage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Xyx
+ *
+ */
+public class DataBaseTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
