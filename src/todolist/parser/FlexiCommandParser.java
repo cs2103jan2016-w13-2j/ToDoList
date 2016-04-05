e43a45bfb6d823204aee0b86c4379125bea1a24c
zhangjiyi <zhangjiyiwinwin@gmail.com>
2016-04-05 20:26:11 +0800

    parser parser

--- src/todolist/parser/FlexiCommandParser.java
+++ src/todolist/parser/FlexiCommandParser.java
@@ -31,33 +31,25 @@ public class FlexiCommandParser {
             || temp[i].equals("remove-recurring")) {
         		return new TokenizedCommand("invalid", temp);
         	} else {
-        		
+        		Parser parser = new Parser(TimeZone.getDefault());
+                List<DateGroup> groups = parser.parse(input);
+                for (DateGroup group : groups) {
+                    List<Date> dates = group.getDates();
+//                    int line = group.getLine();
+//                    int column = group.getPosition();
+//                    String matchingValue = group.getText();
+//                    String syntaxTree = group.getSyntaxTree().toStringTree();
+//                    Map parseMap = group.getParseLocations();
+//                    boolean isRecurreing = group.isRecurring();
+//                    Date recursUntil = group.getRecursUntil();
+                    System.out.println(dates);
+
+                }
+
+                String action = null;
+                String args[] = null;
+                return new TokenizedCommand(action, args);
         	}
         }
-    	
-    	
-    	
-    	
-    	
-    	
-
-        Parser parser = new Parser(TimeZone.getDefault());
-        List<DateGroup> groups = parser.parse(input);
-        for (DateGroup group : groups) {
-            List<Date> dates = group.getDates();
-//            int line = group.getLine();
-//            int column = group.getPosition();
-//            String matchingValue = group.getText();
-//            String syntaxTree = group.getSyntaxTree().toStringTree();
-//            Map parseMap = group.getParseLocations();
-//            boolean isRecurreing = group.isRecurring();
-//            Date recursUntil = group.getRecursUntil();
-            System.out.println(dates);
-
-        }
-
-        String action = null;
-        String args[] = null;
-        return new TokenizedCommand(action, args);
     }
 }
