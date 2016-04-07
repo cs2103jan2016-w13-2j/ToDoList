b359d9072b9a51a53a694ccdac101de4d0aa33f4
zhangjiyi <zhangjiyiwinwin@gmail.com>
2016-04-07 20:07:18 +0800

    refactor

--- src/todolist/parser/FlexiCommandParser.java
+++ src/todolist/parser/FlexiCommandParser.java
@@ -31,25 +31,26 @@ public class FlexiCommandParser {
 					|| temp[i].equals("switchposition") || temp[i].equals("label") || temp[i].equals("postpone")
 					|| temp[i].equals("forward") || temp[i].equals("add-remind") || temp[i].equals("remind")
 					|| temp[i].equals("add-remind-bef") || temp[i].equals("remind-bef") || temp[i].equals("done")
-					|| temp[i].equals("undone") || temp[i].equals("exit") || temp[i].equals("undo") || temp[i].equals("redo")
-					|| temp[i].equals("reset") || temp[i].equals("tab") || temp[i].equals("set-recurring")
-					|| temp[i].equals("remove-recurring") || temp[i].equals("create") || temp[i].equals("schedule")
-					|| temp[i].equals("cancel") || temp[i].equals("remove") || temp[i].equals("modify") || temp[i].equals("change")
-					|| temp[i].equals("replace") || temp[i].equals("archive") || temp[i].equals("complete") || temp[i].equals("finish")
+					|| temp[i].equals("undone") || temp[i].equals("exit") || temp[i].equals("undo")
+					|| temp[i].equals("redo") || temp[i].equals("reset") || temp[i].equals("tab")
+					|| temp[i].equals("set-recurring") || temp[i].equals("remove-recurring") || temp[i].equals("create")
+					|| temp[i].equals("schedule") || temp[i].equals("cancel") || temp[i].equals("remove")
+					|| temp[i].equals("modify") || temp[i].equals("change") || temp[i].equals("replace")
+					|| temp[i].equals("archive") || temp[i].equals("complete") || temp[i].equals("finish")
 					|| temp[i].equals("shelf") || temp[i].equals("unarchive") || temp[i].equals("incomplete")
-					|| temp[i].equals("unfinish") || temp[i].equals("unshelf") || temp[i].equals("delay") || temp[i].equals("advance")
-					|| temp[i].equals("categorize") || temp[i].equals("tag") || temp[i].equals("load") || temp[i].equals("save") ||temp[i].equals("open")) {
+					|| temp[i].equals("unfinish") || temp[i].equals("unshelf") || temp[i].equals("delay")
+					|| temp[i].equals("advance") || temp[i].equals("categorize") || temp[i].equals("tag")
+					|| temp[i].equals("load") || temp[i].equals("save") || temp[i].equals("open")) {
 
 				return new TokenizedCommand("invalid", temp);
 			}
 		}
-		
-		
+
 		Parser parser = new Parser(TimeZone.getTimeZone(ZoneOffset.systemDefault()));
 		List<DateGroup> groups = parser.parse(input);
 		List<Date> dates = null;
-		int[] column = new int[] {0, 0};
-		int[] length = new int[] {0, 0};
+		int[] column = new int[] { 0, 0 };
+		int[] length = new int[] { 0, 0 };
 		int counter = 0;
 		for (DateGroup group : groups) {
 			dates = group.getDates();
@@ -64,116 +65,132 @@ public class FlexiCommandParser {
 			length[counter] = group.getText().length();
 			counter++;
 		}
-		
+
 		System.out.println(dates);
 		System.out.println(column[0] + " " + length[0]);
 		System.out.println(column[1] + " " + length[1]);
-		
+
 		String result = null;
-		
-		
-		if(dates == null || dates.size() == 0) {
-			return new TokenizedCommand("add", new String[]{"task", input});
+
+		if (dates == null || dates.size() == 0) {
+			return new TokenizedCommand("add", new String[] { "task", input });
 		} else {
-			if(column[1] == 0) {
+			if (column[1] == 0) {
 				System.out.println(input.substring(0, column[0] - 1));
-				System.out.println(input.substring(column[0] + length[0] -1));
-			
-				
-				result = input.substring(0, column[0] - 1) + input.substring(column[0] + length[0] -1);
+				System.out.println(input.substring(column[0] + length[0] - 1));
+
+				result = input.substring(0, column[0] - 1) + input.substring(column[0] + length[0] - 1);
 			} else {
-				result = input.substring(0, column[0] - 1) + input.substring(column[0] + length[0], column[1] - 1) + input.substring(column[1] + length[1] - 1);
+				result = input.substring(0, column[0] - 1) + input.substring(column[0] + length[0], column[1] - 1)
+						+ input.substring(column[1] + length[1] - 1);
 			}
-			
-			if(dates.size() == 1) {
+
+			if (dates.size() == 1) {
 				Date deadline = dates.get(0);
-				
-				
+
 				Instant instant = Instant.ofEpochMilli(deadline.getTime());
-			    LocalDateTime end = LocalDateTime.ofInstant(instant, ZoneOffset.systemDefault());
-			    
-			    DecimalFormat decimalFormatter = new DecimalFormat("00");
-			    String deadlineDate = end.getYear() + "-"
-						+ decimalFormatter.format(end.getMonthValue()) + "-"
+				LocalDateTime end = LocalDateTime.ofInstant(instant, ZoneOffset.systemDefault());
+
+				DecimalFormat decimalFormatter = new DecimalFormat("00");
+				String deadlineDate = end.getYear() + "-" + decimalFormatter.format(end.getMonthValue()) + "-"
 						+ decimalFormatter.format(end.getDayOfMonth());
-			    
-			    String deadlineTime = decimalFormatter.format(end.getHour()) + ":" + decimalFormatter.format(end.getMinute());
-			    
-			    
-			    if(end.isBefore(LocalDateTime.now()) && end.plus(10, ChronoUnit.SECONDS).isAfter(LocalDateTime.now()) && input.toLowerCase().contains("today")) {
-			    	//System.out.println("hello");
-				    deadlineTime = "23:59";
-			    }
-			    
-			    if(result.endsWith(" by ")) {
-			    	result = result.substring(0, result.length() - 3);
-			    }
-			    
-			    if(result.endsWith(" from ")) {
-			    	result = result.substring(0, result.length() - 5);
-			    }
-			    
-			    if(result.startsWith("From ") || result.startsWith("from ")) {
-			    	result = result.substring(6);
-			    }
-			    
-			    /*
-				String endDate = deadline.getYear() + "-" + deadline.getMonth() + "-" + deadline.getDay();
-				String endTime = deadline.getHours() + ":" + deadline.getMinutes();
-				return new TokenizedCommand("add", new String[]{"deadline", input, endDate, endTime});
-				*/
-			    
-				return new TokenizedCommand("add", new String[]{"deadline", result, deadlineDate, deadlineTime});
+
+				String deadlineTime = decimalFormatter.format(end.getHour()) + ":"
+						+ decimalFormatter.format(end.getMinute());
+
+				if (end.isBefore(LocalDateTime.now()) && end.plus(10, ChronoUnit.SECONDS).isAfter(LocalDateTime.now())
+						&& input.toLowerCase().contains("today")) {
+					// System.out.println("hello");
+					deadlineTime = "23:59";
+				}
+
+				result = keywordFilter(result);
+
+				if (result.toLowerCase().contains("breakfast")) {
+					deadlineTime = "09:00";
+				}
+
+				if (result.toLowerCase().contains("lunch")) {
+					deadlineTime = "13:00";
+				}
+
+				if (result.toLowerCase().contains("dinner")) {
+					deadlineTime = "19:00";
+				}
+				if (result.toLowerCase().contains("supper")) {
+					deadlineTime = "23:00";
+				}
+
+				/*
+				 * String endDate = deadline.getYear() + "-" +
+				 * deadline.getMonth() + "-" + deadline.getDay(); String endTime
+				 * = deadline.getHours() + ":" + deadline.getMinutes(); return
+				 * new TokenizedCommand("add", new String[]{"deadline", input,
+				 * endDate, endTime});
+				 */
+
+				return new TokenizedCommand("add", new String[] { "deadline", result, deadlineDate, deadlineTime });
 
 			} else {
-			    DecimalFormat decimalFormatter = new DecimalFormat("00");
+				DecimalFormat decimalFormatter = new DecimalFormat("00");
 
-			    
 				Date startTimeOriginal = dates.get(0);
 				Date endTimeOriginal = dates.get(1);
-				
+
 				Instant startInstant = Instant.ofEpochMilli(startTimeOriginal.getTime());
-			    LocalDateTime start = LocalDateTime.ofInstant(startInstant, ZoneId.systemDefault());
-			    
-			    //Instant endInstant = Instant.ofEpochMilli(endTimeOriginal.getTime());
-			    //LocalDateTime end = LocalDateTime.ofInstant(endInstant, ZoneId.systemDefault());
-			    
-			    String startDate = start.getYear() + "-"
-						+ decimalFormatter.format(start.getMonthValue()) + "-"
+				LocalDateTime start = LocalDateTime.ofInstant(startInstant, ZoneId.systemDefault());
+
+				// Instant endInstant =
+				// Instant.ofEpochMilli(endTimeOriginal.getTime());
+				// LocalDateTime end = LocalDateTime.ofInstant(endInstant,
+				// ZoneId.systemDefault());
+
+				String startDate = start.getYear() + "-" + decimalFormatter.format(start.getMonthValue()) + "-"
 						+ decimalFormatter.format(start.getDayOfMonth());
-			    
-			    String startTime = decimalFormatter.format(start.getHour()) + ":" + decimalFormatter.format(start.getMinute());
-			    
-			    int interval = (int) getDateDiff(startTimeOriginal, endTimeOriginal)/1000/60;
-			    
+
+				String startTime = decimalFormatter.format(start.getHour()) + ":"
+						+ decimalFormatter.format(start.getMinute());
+
+				int interval = (int) getDateDiff(startTimeOriginal, endTimeOriginal) / 1000 / 60;
+
 				/*
-				String startDate = start.getYear() + "-" + start.getMonth() + "-" + start.getDay();
-				String startTime = start.getHours() + ":" + start.getMinutes();
-				
-				String endDate = end.getYear() + "-" + end.getMonth() + "-" + end.getDay();
-				String endTime = end.getHours() + ":" + end.getMinutes();
-				*/
-			    
-				//return new TokenizedCommand("add", new String[]{"event", input, startDate, startTime, endDate, endTime});
-			    
-			    if(result.endsWith(" by ")) {
-			    	result = result.substring(0, result.length() - 3);
-			    }
-			    
-			    if(result.endsWith(" from ")) {
-			    	result = result.substring(0, result.length() - 5);
-			    }
-			    
-			    if(result.startsWith("From ") || result.startsWith("from ")) {
-			    	result = result.substring(6);
-			    }
-			    
-				return new TokenizedCommand("add", new String[]{"event", result, startDate, startTime, Integer.toString(interval), "minute"});
+				 * String startDate = start.getYear() + "-" + start.getMonth() +
+				 * "-" + start.getDay(); String startTime = start.getHours() +
+				 * ":" + start.getMinutes();
+				 * 
+				 * String endDate = end.getYear() + "-" + end.getMonth() + "-" +
+				 * end.getDay(); String endTime = end.getHours() + ":" +
+				 * end.getMinutes();
+				 */
+
+				// return new TokenizedCommand("add", new String[]{"event",
+				// input, startDate, startTime, endDate, endTime});
+
+				result = keywordFilter(result);
+
+				return new TokenizedCommand("add",
+						new String[] { "event", result, startDate, startTime, Integer.toString(interval), "minute" });
 			}
 		}
 	}
+	
+	private String keywordFilter(String input) {
+		String result = input;
+		if (result.endsWith(" by ")) {
+			result = result.substring(0, result.length() - 3);
+		}
 
-	private static long getDateDiff(Date date1, Date date2) {
+		if (result.endsWith(" from ")) {
+			result = result.substring(0, result.length() - 5);
+		}
+
+		if (result.startsWith("From ") || result.startsWith("from ")) {
+			result = result.substring(6);
+		}
+		return result;
+	}
+
+	private long getDateDiff(Date date1, Date date2) {
 		long diffInMillies = date2.getTime() - date1.getTime();
 		return diffInMillies;
 	}
